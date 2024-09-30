2e73a7eeac9d10f327181d08b1b23bbdfee1f457
Abdalrahman Eissa <abdalrahmaneissaa@gmail.com>
2024-09-30 10:03:22 +0300

    Add Base.mk Inherit Product in device.mk

--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
 # API levels
 PRODUCT_SHIPPING_API_LEVEL := 30
 
