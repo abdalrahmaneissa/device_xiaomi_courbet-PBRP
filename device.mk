6711eb7b253c02e36cc2463a52a73ad315408514
Abdalrahman Eissa <abdalrahmaneissaa@gmail.com>
2024-09-30 09:36:04 +0300

    Add Screen Density Specifics

--- device.mk
+++ device.mk
@@ -7,6 +7,10 @@
 # API levels
 PRODUCT_SHIPPING_API_LEVEL := 30
 
+# Screen Density
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+
 # fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.1-impl-mock \
