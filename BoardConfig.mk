976b808751f500b611c39c0dad56e276bec60e16
Abdalrahman Eissa <abdalrahmaneissaa@gmail.com>
2024-10-08 11:56:26 +0300

    Remove Default Recovery Brightness

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -109,7 +109,6 @@ TW_INCLUDE_RESETPROP := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
 TW_OVERRIDE_SYSTEM_PROPS := "ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental"
-TW_DEFAULT_BRIGHTNESS := 1200
 TW_Y_OFFSET := 91
 TW_H_OFFSET := -91
 TWRP_INCLUDE_LOGCAT := true
